e6ac24686eb7b56d65c511dae4d43451e85b0f77
yanbahiano07-hue <yanbahiano07@gmail.com>
2026-05-22 10:51:51 -0300

    atualizando

--- Dockerfile
+++ Dockerfile
@@ -8,3 +8,4 @@ COPY . /var/www/html/
 RUN chown -R www-data:www-data /var/www/html
 
 EXPOSE 80
+
--- assets/js/app.js
+++ assets/js/app.js
@@ -158,3 +158,4 @@ document.addEventListener('click', (e) => {
         startAutoPlay();
     }
 }); 
+
