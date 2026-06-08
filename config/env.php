f2e1dfe3209228b6c4774b1429e92313a76031ea
yanbahiano07-hue <yanbahiano07@gmail.com>
2026-06-08 20:30:46 -0300

    docker

---
+++ config/env.php
@@ -0,0 +1,19 @@
+<?php
+
+$envFile = __DIR__ . '/../.env';
+
+if (file_exists($envFile)) {
+
+    $lines = file($envFile, FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
+
+    foreach ($lines as $line) {
+
+        if (strpos(trim($line), '#') === 0) {
+            continue;
+        }
+
+        list($key, $value) = explode('=', $line, 2);
+
+        $_ENV[trim($key)] = trim($value);
+    }
+}
