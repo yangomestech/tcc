f576b680d4f04ccd3d3342c33a22b18dc59c5d6b
yangomestech <yangomestech@gmail.com>
2026-07-01 03:56:25 -0300

    fix: simbolos esquecidos da pagina de busca

--- views/busca.php
+++ views/busca.php
@@ -97,13 +97,30 @@ try {
   </a>
   <nav class="nav-links nav-desktop">
     <?php if ($logado): ?>
-        <a href="../controllers/evento-process.php" class="nav-item">Criar evento</a>
-        <a href="../views/meus-eventos.php" class="nav-item">Meus eventos</a>
+<?php if (!empty($_SESSION['documentos_completos'])): ?>
+                      <a href="../controllers/evento-process.php" class="nav-item">
+                          <svg viewBox="0 0 24 24" width="20" height="20"><path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm5 11h-4v4h-2v-4H7v-2h4V7h2v4h4v2z" fill="currentColor"/></svg> Criar evento
+                      </a>
+                  <?php else: ?>
+                      <a href="#" class="nav-item" onclick="abrirModalDocumentos(event)">
+                          <svg viewBox="0 0 24 24" width="20" height="20"><path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm5 11h-4v4h-2v-4H7v-2h4V7h2v4h4v2z" fill="currentColor"/></svg> Criar evento
+                      </a>
+                  <?php endif; ?>
+        <a href="../views/meus-eventos.php" class="nav-item">
+          <svg viewBox="0 0 24 24" width="20" height="20"><path d="M19 4h-1V2h-2v2H8V2H6v2H5c-1.11 0-1.99.9-1.99 2L3 20c0 1.1.89 2 2 2h14c1.1 0 2-.9 2-2V6c0-1.1-.9-2-2-2zm0 16H5V10h14v10z" fill="currentColor"/></svg>
+          Meus eventos
+        </a>
+        <a href="../controllers/eventos-confirmados.php" class="nav-item">
+          <svg viewBox="0 0 24 24" width="20" height="20"><path d="M22 10V6c0-1.11-.9-2-2-2H4c-1.1 0-1.99.89-1.99 2v4c1.1 0 1.99.9 1.99 2s-.89 2-2 2v4c0 1.1.9 2 2 2h16c1.1 0 2-.9 2-2v-4c-1.1 0-2-.9-2-2s.9-2 2-2zm-2 7.74c-1.75-.8-3-2.58-3-4.74s1.25-3.94 3-4.74V6H4v2.26c1.75.8 3 2.58 3 4.74s-1.25 3.94-3 4.74V18h16v-.26z" fill="currentColor"/></svg>
+          Eventos confirmados
+        </a>
+
         <div class="user-menu-container">
           <button class="user-profile-btn" id="userMenuBtn">
-            <svg viewBox="0 0 24 24" width="24" height="24"><path d="M3 18h18v-2H3v2zm0-5h18v-2H3v2zm0-7v2h18V6H3z" fill="currentColor"/></svg>
+            <svg class="hamburger-icon" viewBox="0 0 24 24" width="24" height="24"><path d="M3 18h18v-2H3v2zm0-5h18v-2H3v2zm0-7v2h18V6H3z" fill="currentColor"/></svg>
             <div class="user-initials"><?= $initials; ?></div>
           </button>
+
           <div class="user-dropdown" id="userDropdown">
             <div class="dropdown-header">
               <div class="user-initials-large"><?= $initials; ?></div>
@@ -112,11 +129,30 @@ try {
                 <span><?= htmlspecialchars($email, ENT_QUOTES, 'UTF-8'); ?></span>
               </div>
             </div>
+
             <ul class="dropdown-list">
-              <li><a href="../controllers/logout-process.php" class="logout-link">Sair</a></li>
+              <li><a href="../views/usuario.php"><svg viewBox="0 0 24 24" width="20" height="20"><path d="M12 12c2.21 0 4-1.79 4-4s-1.79-4-4-4-4 1.79-4 4 1.79 4 4 4zm0 2c-2.67 0-8 1.34-8 4v2h16v-2c0-2.66-5.33-4-8-4z" fill="currentColor"/></svg> Minha conta</a></li>
+              <li><a href="../controllers/favoritos-process.php"><svg viewBox="0 0 24 24" width="20" height="20"><path d="M12 21.35l-1.45-1.32C5.4 15.36 2 12.28 2 8.5 2 5.42 4.42 3 7.5 3c1.74 0 3.41.81 4.5 2.09C13.09 3.81 14.76 3 16.5 3 19.58 3 22 5.42 22 8.5c0 3.78-3.4 6.86-8.55 11.54L12 21.35z" fill="currentColor"/></svg> Favoritos</a></li>
+<li>
+                  <?php if (!empty($_SESSION['documentos_completos'])): ?>
+                      <a href="../controllers/evento-process.php">
+                          <svg viewBox="0 0 24 24" width="20" height="20"><path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm5 11h-4v4h-2v-4H7v-2h4V7h2v4h4v2z" fill="currentColor"/></svg> Criar evento
+                      </a>
+                  <?php else: ?>
+                      <a href="#" onclick="abrirModalDocumentos(event)">
+                          <svg viewBox="0 0 24 24" width="20" height="20"><path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm5 11h-4v4h-2v-4H7v-2h4V7h2v4h4v2z" fill="currentColor"/></svg> Criar evento
+                      </a>
+                  <?php endif; ?>
+              </li>
+              <li><a href="#"><svg viewBox="0 0 24 24" width="20" height="20"><path d="M19 4h-1V2h-2v2H8V2H6v2H5c-1.11 0-1.99.9-1.99 2L3 20c0 1.1.89 2 2 2h14c1.1 0 2-.9 2-2V6c0-1.1-.9-2-2-2zm0 16H5V10h14v10z" fill="currentColor"/></svg> Meus eventos</a></li>
+              <li class="divider"></li>
+              <li><a href="#"><svg viewBox="0 0 24 24" width="20" height="20"><path d="M11 18h2v-2h-2v2zm1-16C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 18c-4.41 0-8-3.59-8-8s3.59-8 8-8 8 3.59 8 8-3.59 8-8 8zm0-14c-2.21 0-4 1.79-4 4h2c0-1.1.9-2 2-2s2 .9 2 2c0 2-3 1.75-3 5h2c0-2.25 3-2.5 3-5 0-2.21-1.79-4-4-4z" fill="currentColor"/></svg> Suporte</a></li>
+              <li class="divider"></li>
+              <li><a href="../controllers/logout-process.php" class="logout-link"><svg viewBox="0 0 24 24" width="20" height="20"><path d="M17 7l-1.41 1.41L18.17 11H8v2h10.17l-2.58 2.58L17 17l5-5zM4 5h8V3H4c-1.1 0-2 .9-2 2v14c0 1.1.9 2 2 2h8v-2H4V5z" fill="currentColor"/></svg> Sair</a></li>
             </ul>
           </div>
         </div>
+
     <?php else: ?>
         <a href="../views/login.php" class="nav-item" style="font-weight: 600;">Entrar</a>
         <a href="../views/cadastro.php" class="nav-item" style="background: #ff5e00; color: #fff; padding: 8px 20px; border-radius: 24px; font-weight: 600;">Criar conta</a>
@@ -140,11 +176,19 @@ try {
     <svg class="chevron-icon" viewBox="0 0 24 24"><path d="M7 10l5 5 5-5z" fill="currentColor"/></svg>
     
     <ul class="location-menu" id="locationMenu">
+      <li class="location-item use-location" data-value="gps">
+        <svg viewBox="0 0 24 24" width="16" height="16"><path d="M12 8c-2.21 0-4 1.79-4 4s1.79 4 4 4 4-1.79 4-4-1.79-4-4-4zm8.94 3c-.46-4.17-3.77-7.48-7.94-7.94V1h-2v2.06C6.83 3.52 3.52 6.83 3.06 11H1v2h2.06c.46 4.17 3.77 7.48 7.94 7.94V23h2v-2.06c4.17-.46 7.48-3.77 7.94-7.94H23v-2h-2.06zM12 19c-3.87 0-7-3.13-7-7s3.13-7 7-7 7 3.13 7 7-3.13 7-7 7z" fill="currentColor"/></svg>
+        Usar minha localização atual
+      </li>
       <li class="location-item" data-value="">Qualquer lugar</li>
       <li class="location-item" data-value="SP">São Paulo (SP)</li>
+      <li class="location-item" data-value="SC">Santa Catarina (SC)</li>
       <li class="location-item" data-value="RJ">Rio de Janeiro (RJ)</li>
+      <li class="location-item" data-value="PR">Paraná (PR)</li>
+      <li class="location-item" data-value="MG">Minas Gerais (MG)</li>
     </ul>
   </div>
+  
   <button type="submit" style="display: none;">Buscar</button>
 </form>
 
