afe2591b96eab17d2c014216f4aa0c83433d2926
yangomestech <yangomestech@gmail.com>
2026-07-01 05:07:47 -0300

    fix: reformulação do front-end da pagina de busca

--- views/busca.php
+++ views/busca.php
@@ -89,7 +89,7 @@ try {
   <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;600&display=swap" rel="stylesheet">
   <link rel="stylesheet" href="../assets/css/styleDashboard.css">
 </head>
-<body>
+<body class="busca-page">
 
 <header class="header-sympla">
   <a href="../controllers/dashboard-process.php" class="logo">
